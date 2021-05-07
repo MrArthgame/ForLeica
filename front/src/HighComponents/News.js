d6cfb684c52e601181890a97978433a6e9078954
overlord0000 <vahan.muradyan@beeoncode.com>
2021-05-07 23:33:38 +0400

    changes

---
+++ front/src/HighComponents/News.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ForNews from '../components/ForNews/ForNews'
+
+export default function News() {
+    return (
+        <div>
+            <div style={{display:'flex',justifyContent:'center'}}>
+                <h1>News</h1>
+            </div>
+            
+            <ForNews/>
+        </div>
+    )
+}
